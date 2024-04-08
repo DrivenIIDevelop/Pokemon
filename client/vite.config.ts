9a5a67370e2c9220c696aee090f1b9243d6d2e58
Ben Keys <bkeys818@gmail.com>
2024-04-08 12:29:55 -0400

    🔧 Configure API proxy (dev only)

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  const envDir = resolve(__dirname, '../')
+  const env = loadEnv(mode, envDir, '')
+  return {
+    envDir,
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: `http://${env.VITE_API_HOST}:${env.VITE_API_PORT}/`,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+  }
 })
