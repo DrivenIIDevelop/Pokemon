a7d9d1fff1681040eacd7dcee4c8f2f474dc327f
Ben Keys <bkeys818@gmail.com>
2024-04-08 12:32:46 -0400

    🚧 Make demo API call

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [title, setTitle] = useState<string>("")
+
+  useEffect(() => {
+    getTitle()
+  })
+
+  async function getTitle() {
+    const res = await fetch('/api')
+    if (res.ok) {
+      const { title } = await res.json()
+      setTitle(title)
+    } else {
+      console.error(`${res.statusText} (${res.status})`)
+    }
+  }
 
   return (
     <>
@@ -16,15 +30,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>{title}</h1>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
