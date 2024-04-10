8d8736ffbbe16de4ecb70ec05e4725dcf82195c4
Ben Keys <bkeys818@gmail.com>
2024-04-10 12:36:31 -0400

    🎨 Format code

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,38 +4,36 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [title, setTitle] = useState<string>("")
+    const [title, setTitle] = useState<string>('')
 
-  useEffect(() => {
-    getTitle()
-  })
+    useEffect(() => {
+        getTitle()
+    })
 
-  async function getTitle() {
-    const res = await fetch('/api')
-    if (res.ok) {
-      const { title } = await res.json()
-      setTitle(title)
-    } else {
-      console.error(`${res.statusText} (${res.status})`)
+    async function getTitle() {
+        const res = await fetch('/api')
+        if (res.ok) {
+            const { title } = await res.json()
+            setTitle(title)
+        } else {
+            console.error(`${res.statusText} (${res.status})`)
+        }
     }
-  }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>{title}</h1>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <div>
+                <a href="https://vitejs.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
+            <h1>{title}</h1>
+            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+        </>
+    )
 }
 
 export default App
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,20 +4,20 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const envDir = resolve(__dirname, '../')
-  const env = loadEnv(mode, envDir, '')
-  return {
-    envDir,
-    plugins: [react()],
-    server: {
-      proxy: {
-        "/api": {
-          target: `http://${env.VITE_API_HOST}:${env.VITE_API_PORT}/`,
-          changeOrigin: true,
-          secure: false,
-          rewrite: (path) => path.replace(/^\/api/, ""),
+    const envDir = resolve(__dirname, '../')
+    const env = loadEnv(mode, envDir, '')
+    return {
+        envDir,
+        plugins: [react()],
+        server: {
+            proxy: {
+                '/api': {
+                    target: `http://${env.VITE_API_HOST}:${env.VITE_API_PORT}/`,
+                    changeOrigin: true,
+                    secure: false,
+                    rewrite: path => path.replace(/^\/api/, ''),
+                },
+            },
         },
-      },
-    },
-  }
+    }
 })
