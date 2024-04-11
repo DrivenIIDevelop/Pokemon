66670ad845092c7da3cfb52302494fb77c7b1aa9
Ben Keys <bkeys818@gmail.com>
2024-04-11 11:16:27 -0400

    🔧 Configure vite-plugin-remix-router

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
+import RemixRouter from 'vite-plugin-remix-router'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -8,7 +9,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, envDir, '')
     return {
         envDir,
-        plugins: [react()],
+        plugins: [react(), RemixRouter()],
         server: {
             proxy: {
                 '/api': {
