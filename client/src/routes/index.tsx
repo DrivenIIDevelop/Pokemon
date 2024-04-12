b812b007f09dd3b87de745cc1a1bd204bbe8b652
Ben Keys <bkeys818@gmail.com>
2024-04-12 10:21:12 -0400

    🩹 Fix linter errors

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -7,13 +7,13 @@ export function Component() {
     const [title, setTitle] = useState<string>('')
 
     useEffect(() => {
-        getTitle()
+        void getTitle()
     })
 
     async function getTitle() {
         const res = await fetch('/api')
         if (res.ok) {
-            const { title } = await res.json()
+            const { title } = (await res.json()) as { title: string }
             setTitle(title)
         } else {
             console.error(`${res.statusText} (${res.status})`)
@@ -23,10 +23,10 @@ export function Component() {
     return (
         <>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
                     <img src={viteLogo} className="logo" alt="Vite logo" />
                 </a>
-                <a href="https://react.dev" target="_blank">
+                <a href="https://react.dev" target="_blank" rel="noreferrer">
                     <img src={reactLogo} className="logo react" alt="React logo" />
                 </a>
             </div>
