b59442b9dc65eef254fc06babcb5fb3446dbfb17
Ben Keys <bkeys818@gmail.com>
2024-04-17 11:25:53 -0400

    🔥 Remove demo code

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,7 +1,4 @@
 import { useState, useEffect } from 'react'
-import reactLogo from '../assets/react.svg'
-import viteLogo from '/vite.svg'
-import '../App.css'
 
 export function Component() {
     const [title, setTitle] = useState<string>('')
@@ -20,18 +17,5 @@ export function Component() {
         }
     }
 
-    return (
-        <>
-            <div>
-                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank" rel="noreferrer">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
-            </div>
-            <h1>{title}</h1>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-        </>
-    )
+    return <h1>{title}</h1>
 }
