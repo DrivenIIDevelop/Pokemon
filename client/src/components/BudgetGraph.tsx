2883c03dfd086e7a2dedabdb63fa1c495106a8d8
Ben Keys <bkeys818@gmail.com>
2024-04-23 11:58:30 -0400

    🚧 Create basic mode switcher button

---
+++ client/src/components/BudgetGraph.tsx
@@ -0,0 +1,7 @@
+import { useContext } from 'react'
+import BudgetModeContext from '../contexts/BudgetModeContext'
+
+export default function BudgetGraph() {
+    const mode = useContext(BudgetModeContext)
+    return <h1>Budget Mode: {mode}</h1>
+}
--- client/src/routes/budget/index.tsx
+++ client/src/routes/budget/index.tsx
@@ -1,3 +1,35 @@
+import { useState } from 'react'
+import Box from '@mui/material/Box'
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup'
+import ToggleButton from '@mui/material/ToggleButton'
+import BudgetModeContext, { BudgetMode } from '../../contexts/BudgetModeContext'
+import BudgetGraph from '../../components/BudgetGraph'
+
 export function Component() {
-    return <h1>Budget</h1>
+    const [mode, setMode] = useState<BudgetMode>('budget')
+
+    return (
+        <BudgetModeContext.Provider value={mode}>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                    width: '100dvw',
+                }}
+            >
+                <ToggleButtonGroup
+                    value={mode}
+                    onChange={(_, value) => setMode(value)}
+                    exclusive
+                    size="small"
+                    sx={{ backgroundColor: 'primary.main' }}
+                >
+                    <ToggleButton value="budget">Budget</ToggleButton>
+                    <ToggleButton value="expenses">Expenses</ToggleButton>
+                </ToggleButtonGroup>
+                <BudgetGraph />
+            </Box>
+        </BudgetModeContext.Provider>
+    )
 }
