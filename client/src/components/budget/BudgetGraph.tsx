41b6b23b504208c49c642e1ac07fab457a3276e4
Ben Keys <bkeys818@gmail.com>
2024-04-24 11:43:49 -0400

    💩 Create temporary graph standin

--- client/src/components/budget/BudgetGraph.tsx
+++ client/src/components/budget/BudgetGraph.tsx
@@ -1,7 +1,10 @@
-import { useContext } from 'react'
-import BudgetModeContext from '../../contexts/BudgetModeContext'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 
 export default function BudgetGraph() {
-  const mode = useContext(BudgetModeContext)
-  return <h1>Budget Mode: {mode}</h1>
+  return (
+    <Box width="100%" height="200px" border="1px solid grey" display="flex" alignItems="center" justifyContent="center">
+      <Typography variant="h5">Budget Graph</Typography>
+    </Box>
+  )
 }
