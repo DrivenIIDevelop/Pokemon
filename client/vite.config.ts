f15b2ed5948ecd13407642ed7a4fdc43fd956dc5
Ben Keys <bkeys818@gmail.com>
2024-05-01 11:30:05 -0400

    🔧 Add path aliase to tsconfig

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import { resolve } from 'path'
 import react from '@vitejs/plugin-react-swc'
 import RemixRouter from 'vite-plugin-remix-router'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,7 +10,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, envDir, '')
   return {
     envDir,
-    plugins: [react(), RemixRouter()],
+    plugins: [react(), RemixRouter(), tsconfigPaths()],
     server: {
       proxy: {
         '/api': {
