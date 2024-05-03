c744baab8d255b017df0b917567f60fb8d6dc70b
Kyle O'Neill <oneil.20kyle@gmail.com>
2024-05-03 14:50:55 -0400

    Update Header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,89 +1,146 @@
 import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
+import CssBaseline from '@mui/material/CssBaseline'
+import Divider from '@mui/material/Divider'
+import Drawer from '@mui/material/Drawer'
+import IconButton from '@mui/material/IconButton'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import MenuIcon from '@mui/icons-material/Menu'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
-import IconButton from '@mui/material/IconButton'
-import MenuIcon from '@mui/icons-material/Menu'
-import AccountCircle from '@mui/icons-material/AccountCircle'
-import Switch from '@mui/material/Switch'
-import FormControlLabel from '@mui/material/FormControlLabel'
-import FormGroup from '@mui/material/FormGroup'
-//import Menu from '@mui/material/Menu'
-import { Drawer, ListItemButton, ListItemText, ListItem, List } from '@mui/material'
+import Button from '@mui/material/Button'
+import { ThemeProvider } from '@emotion/react'
+import theme from '../theme'
+import { CreditCardOutlined, ExitToApp, QuizOutlined, ReceiptLongOutlined, SavingsOutlined } from '@mui/icons-material'
+import { Badge } from '@mui/material'
 
-export function Header() {
-  const [auth, setAuth] = React.useState(true)
-  const [open, setOpen] = React.useState(false)
-  const toggleDrawer = (newOpen: boolean) => () => {
-    setOpen(newOpen)
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window
+}
+
+const drawerWidth = 240
+const navItems = ['Budget', 'Accounts', 'Quiz', 'Transacrtion History', 'Sort Expenses']
+
+export default function DrawerAppBar(props: Props) {
+  const { window } = props
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(prevState => !prevState)
   }
-  /*This block of code creates the list that is in the drawer popout component, 
-it needs to be changed so it has the full functionality to take it to the different pages*/
-  const DrawerList = (
-    <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ alignContent: 'flex-start' }}>
+      <List sx={{ paddingLeft: '10px' }}>
+        <ListItem>Windfall</ListItem>
+
+        <ListItem>
+          <SavingsOutlined />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText>Budget</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <CreditCardOutlined />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText>Accounts</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <QuizOutlined />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText>Quiz</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <ReceiptLongOutlined />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText>Transaction History</ListItemText>
+          </ListItemButton>
+        </ListItem>
+        <ListItem>
+          <Badge
+            color="warning"
+            badgeContent="3"
+            anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+            sx={{ paddingLeft: '9px' }}
+          />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText sx={{ paddingLeft: '15px' }}>Sort Expenses</ListItemText>
+          </ListItemButton>
+        </ListItem>
+      </List>
+      <Divider />
       <List>
-        {['*House Icon goes here*', 'Financials', 'Calender', 'Account Settings'].map(text => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem sx={{ paddingLeft: '24px' }}>
+          <ExitToApp />
+          <ListItemButton sx={{ textAlign: 'flex-start' }}>
+            <ListItemText>Sign Out</ListItemText>
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   )
 
-  // const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null)
+  const container = window !== undefined ? () => window().document.body : undefined
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setAuth(event.target.checked)
-  }
-
-  // const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
-  //   setAnchorEl(event.currentTarget)
-  // }
-
-  // const handleClose = () => {
-  //   setAnchorEl(null)
-  // }
-
-  /*this is the actual NavBar scripts*/
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login-switch" />}
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleDrawer(true)}
+    <ThemeProvider theme={theme}>
+      <Box sx={{ display: 'flex' }}>
+        <CssBaseline />
+        <AppBar component="nav">
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              sx={{ mr: 2, display: { sm: 'none' } }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: { xs: 'flex', sm: 'block' } }}>
+              Windfall
+            </Typography>
+            <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+              {navItems.map(item => (
+                <Button key={item} sx={{ color: '#fff' }}>
+                  {item}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+        <nav>
+          <Drawer
+            container={container}
+            variant="temporary"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+            sx={{
+              display: { xs: 'block', sm: 'none' },
+              '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+            }}
           >
-            <MenuIcon />
-          </IconButton>
-          <Drawer open={open} onClose={toggleDrawer(false)}>
-            {DrawerList}
+            {drawer}
           </Drawer>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            *icon goes here* Windfall
-          </Typography>
-          {auth && (
-            <div>
-              <IconButton size="large" aria-label="account of current user" color="inherit">
-                <AccountCircle />
-              </IconButton>
-            </div>
-          )}
-        </Toolbar>
-      </AppBar>
-    </Box>
+        </nav>
+        <Box component="main" sx={{ p: 3 }}>
+          <Toolbar />
+        </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
