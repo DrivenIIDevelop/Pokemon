17f726e79656facc3bbb6c8ee4a3e011bafddcd9
Ben Keys <bkeys818@gmail.com>
2024-05-03 15:40:13 -0400

    🚨 Fix missing import

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -1,5 +1,6 @@
 import { Outlet } from 'react-router-dom'
 import Box from '@mui/material/Box'
+import DrawerAppBar from '@components/DrawerAppBar'
 
 /** The main page layout */
 export function Component() {
