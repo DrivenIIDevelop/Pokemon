1de974550c32277269cf15bb2e1f8856d27f8a98
Marvin Xia <marvinxia09@gmail.com>
2024-05-03 14:04:52 -0700

    code refarctor for auth

--- server/src/authServer.ts
+++ server/src/authServer.ts
@@ -1,6 +1,7 @@
 import dotenvFlow from 'dotenv-flow'
 import express, { Request, Response, NextFunction } from 'express'
 import { mongooseConnect } from './lib/mongoose'
+import { Account } from './models/Account'
 import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt'
 
@@ -31,29 +32,23 @@ app.get('/posts', authenticateToken, (req: Request, res: Response) => {
   res.json(posts.filter(post => post.username === user.name))
 })
 
-app.post('/register', (req: Request, res: Response) => {
-  const { username, password }: { username: string; password: string } = req.body
+app.post('/register', async (req: Request, res: Response) => {
+  try {
+    const { username, password }: { username: string; password: string } = req.body;
+    const hash: string = await bcrypt.hash(password, 10);
+    
+    await Account.create({
+      username: username,
+      password: hash,
+    });
 
-  bcrypt
-    .hash(password, 10)
-    .then((hash: string) => {
-      Accounts.create({
-        username: username,
-        password: hash,
-      })
-        .then(() => {
-          res.json('User registered successfully')
-        })
-        .catch((err: Error) => {
-          console.error('Error occurred during registration:', err)
-          res.status(500).json({ error: 'An error occurred during registration.' })
-        })
-    })
-    .catch((err: Error) => {
-      console.error('Error occurred while hashing password:', err)
-      res.status(500).json({ error: 'An error occurred while processing your request.' })
-    })
-})
+    res.json('User registered successfully');
+  } catch (err) {
+    console.error('Error occurred during registration:', err);
+    res.status(500).json({ error: 'An error occurred during registration.' });
+  }
+});
+
 
 app.post('/login', (req: Request, res: Response) => {
   const username: string = req.body.username
